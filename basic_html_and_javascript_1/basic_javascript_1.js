fa7a4855e857e68c7fe8ae1b56983e049beff22f
KhalilNiser <kaliniser@gmail.com>
2024-05-08 17:26:58 -0700

    HTML_Assignments

---
+++ basic_html_and_javascript_1/basic_javascript_1.js
@@ -0,0 +1,8 @@
+// alert( "Hello World! This is a Message from Khalil" ); 
+
+alert( function My_First_Function()
+	{
+		var String = "Kiss Me, I'm Irish!";
+		var result = String.fontcolor( "green" );
+		document.getElementById( "Irish" ).innerHTML = result;
+	});
